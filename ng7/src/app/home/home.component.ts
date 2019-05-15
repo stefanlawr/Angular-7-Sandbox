0073a8a45af30992a4c01cfc9742ee3ce9e0a0cf
Stefan S <stefanlawr@gmail.com>
2019-05-15 00:02:51 -0400

    Configuring styles on a component

--- ng7/src/app/home/home.component.ts
+++ ng7/src/app/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // tslint:disable-next-line:no-inferrable-types
+  h1Style: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  firstClick() {
+    console.log('Clicked!');
+    this.h1Style = true;
+  }
 }
